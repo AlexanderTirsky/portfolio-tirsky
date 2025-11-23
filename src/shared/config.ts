c7e827b6db8287d290b675ba79f690d049cc5203
Alexander_Tirsky <tirsky06@gmail.com>
2025-11-23 19:17:35 +1000

    first commit

---
+++ src/shared/config.ts
@@ -0,0 +1,14 @@
+export const appConfig = {
+  first_name: 'Александр',
+  last_name: 'Тирский',
+  patronymic_name: 'Владимирович',
+  full_name: 'Тирский Александр Владимирович',
+  age: '36',
+  experience: '0.1 год',
+  lib: ['Docker', 'React', 'Next', 'SCSS', 'Tailwind', 'GIT'],
+  language: ['JS', 'HTML', 'CSS', 'SQL'],
+  meta: {
+    title: 'FRONT разработчик',
+    description: '',
+  }
+};
